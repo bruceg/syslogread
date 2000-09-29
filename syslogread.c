1cbf467e933903c7f5f90adf0f8fdbb37bbc4a03
Bruce Guenter <bruce@untroubled.org>
2000-09-29 22:54:44 +0000

    Strip time stamps off syslog messages here instead of after logging.

--- syslogread.c
+++ syslogread.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -5,6 +6,33 @@
 
 extern void error(const char*);
 
+static void handle_msg(char* str, ssize_t len)
+{
+  char* ptr = str;
+  while(str[len-1] == 0 || str[len-1] == '\n')
+    --len;
+  str[len] = '\n';
+  /* Try to strip off the timestamp after the facility/level stamp */
+  if(*ptr++ == '<') {
+    while(isdigit(*ptr))
+      ++ptr;
+      /* syslog date stamps are of the form:
+       * "MMM DD HH:MM:SS "
+       * This is just a quick parse for the non-alpha parts */
+    if(*ptr++ == '>' &&
+       ptr[3] == ' ' &&
+       isdigit(ptr[4]) && isdigit(ptr[5]) && ptr[6] == ' ' &&
+       isdigit(ptr[7]) && isdigit(ptr[8]) && ptr[9] == ':' &&
+       isdigit(ptr[10]) && isdigit(ptr[11]) && ptr[12] == ':' &&
+       isdigit(ptr[13]) && isdigit(ptr[14]) && ptr[15] == ' ') {
+      memcpy(str + 15, str, ptr - str);
+      len -= 15;
+      str += 15;
+    }
+  }
+  write(1, str, len+1);
+}
+  
 void main_loop(unsigned num_sockets, int* sockets)
 {
   int max_socket = 0;
@@ -24,12 +52,8 @@ void main_loop(unsigned num_sockets, int* sockets)
 	ssize_t r = recvfrom(sockets[i], buf, sizeof buf - 1, 0, 0, 0);
 	if(r == -1)
 	  error("Receive from selected socket failed");
-	else {
-	  while(buf[r-1] == 0 || buf[r-1] == '\n')
-	    --r;
-	  buf[r] = '\n';
-	  write(1, buf, r+1);
-	}
+	else
+	  handle_msg(buf, r);
       }
   }
 }
